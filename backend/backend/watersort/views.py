d22492d77074795a8dd5fbd66d935b310ebd0c8a
saobangmath <anhtai2672000@gmail.com>
2024-06-09 23:06:52 +0700

    add api

--- backend/backend/watersort/views.py
+++ backend/backend/watersort/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from rest_framework import status 
 from rest_framework.decorators import api_view
+from .solver import GameState, Bottle
+import json
 
 # Create your views here.
 
@@ -10,4 +12,14 @@ def index(request, index_id):
 
 @api_view(['POST'])
 def generate_solver_plan(request: HttpRequest): 
-    return HttpResponse("Success", status = 200)
+    data = json.loads(request.body)
+    game : GameState = GameState()
+    bottles = data["bottles"]
+    if not isinstance(bottles, list):
+        return HttpResponse("invalid data", status = 404)
+
+    for bottle in bottles: 
+        game.add_bottle(Bottle(capacity = bottle["capacity"], waters = bottle["waters"]))
+
+    plan  = game.solve()
+    return HttpResponse(plan, status = 200)

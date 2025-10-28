87f448e6359aa8d3551953b1126dddd8fe994206
saobangmath <anhtai2672000@gmail.com>
2025-10-28 23:23:03 +0700

    remove config from next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  env: {
-    // NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production' 
-    //   ? 'https://your-backend-url.railway.app' 
-    //   : 'http://localhost:8000'
   }
 };
 

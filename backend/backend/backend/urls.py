7b6ef4968a4080806d968193d3b48d93858ba5ff
saobangmath <anhtai2672000@gmail.com>
2025-10-28 22:49:27 +0700

    add view

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from watersort import views
+from watersort import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
+    path('', views.home, name='home'),
     #path('admin/', admin.site.urls),
     path("water-sort/",include("watersort.urls")),
     path("api/",include("watersort.urls"))

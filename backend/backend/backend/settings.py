8f9c35395d35c6c93c02e67ba7288365917e9314
saobangmath <anhtai2672000@gmail.com>
2025-10-28 17:22:08 +0700

    fix

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'backend.backend.urls'
 
 TEMPLATES = [
     {
@@ -62,7 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.backend.wsgi.application'
 
 
 # No database configuration needed - using stateless API
